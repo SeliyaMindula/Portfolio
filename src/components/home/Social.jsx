2338eb9e5c2dceeb27017fbf49ca5422cb47aa6e
seliyak <seliyak@yaratechnologies.com>
2023-11-01 12:49:04 +0530

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
+        <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://github.com/" className="home__social-icon" target="_blank">
+        <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href="https://www.facebook.com/" className="home__social-icon" target="_blank">
+        <i class="uil uil-facebook-f"></i>
+        </a>
+    </div>
+
+  );
+};
+
+export default Social;
